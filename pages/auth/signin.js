e66bd7905eb5b7e9c95c5f925689898416955807
steven <8312553+sphred5@users.noreply.github.com>
2022-07-16 10:05:29 -0700

    finished authentication:

--- pages/auth/signin.js
+++ pages/auth/signin.js
@@ -1,22 +1,30 @@
 import Header from "../../components/Header.js";
-import { getProviders } from "next-auth/react";
+import { getProviders, signIn } from "next-auth/react";
 
-export default function signIn({ providers }) {
+export default function signin({ providers }) {
   return (
     <>
       <Header />
-      <diiv>
+      <div className="mt-40">
         {Object.values(providers).map((provider) => (
-          <div key={provider.name}>
+          <div key={provider.name} className="flex flex-col items-center">
             <img
+              className="w-52 object-cover"
               src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2f/Google_2015_logo.svg/1920px-Google_2015_logo.svg.png"
               alt="google logo"
             />
-            <p>This website is created for learning purposes</p>
-            <button>Signin with {provider.name}</button>
+            <p className="text-sm italic my-10 text-center">
+              This website is created for learning purposes
+            </p>
+            <button
+              className="bg-red-400 rounded-lg text-white p-3 hover:bg-red-500"
+              onClick={() => signIn(provider.id, { callbackUrl: "/" })}
+            >
+              Signin with {provider.name}
+            </button>
           </div>
         ))}
-      </diiv>
+      </div>
     </>
   );
 }
