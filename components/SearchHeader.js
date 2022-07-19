ef99ba7a78ae1ad0ee6ac9dc3d943c2f57a0b109
steven <8312553+sphred5@users.noreply.github.com>
2022-07-19 07:41:28 -0700

    continued styling search header component

--- components/SearchHeader.js
+++ components/SearchHeader.js
@@ -1,11 +1,16 @@
 import Image from "next/image";
 import { useRouter } from "next/router";
+import { useRef } from "react";
+import { MicrophoneIcon, SearchIcon, XIcon } from "@heroicons/react/solid";
+import User from "./User.js";
 
 export default function SearchHeader() {
   const router = useRouter();
+  const searchInputRef = useRef(null);
+
   return (
     <header className="sticky top-0 bg-white">
-      <div>
+      <div className="flex w-full p-6 items-center">
         <Image
           onClick={() => router.push("/")}
           className="cursor-pointer"
@@ -15,6 +20,17 @@ export default function SearchHeader() {
           height={40}
           width={120}
         />
+        <form className="flex border border-gray-200 rounded-full shadow-lg px-6 py-3 ml-10 mr-5 flex-grow max-w-3xl items-center">
+          <input
+            type="text"
+            defaultValue={router.query.term}
+            ref={searchInputRef}
+          />
+          <XIcon className="h-7" />
+          <MicrophoneIcon className="h-6" />
+          <SearchIcon className="h-6" />
+        </form>
+        <User className="ml-auto" />
       </div>
     </header>
   );
--- components/User.js
+++ components/User.js
@@ -1,13 +1,13 @@
 import { useSession, signIn, signOut } from "next-auth/react";
 
-export default function User() {
+export default function User({ className }) {
   const { data: session } = useSession();
   if (session) {
     return (
       <>
         <img
           onClick={signOut}
-          className="h-10 w-10 rounded-full hover:bg-gray-200 cursor-pointer p-1"
+          className={`h-10 w-10 rounded-full hover:bg-gray-200 cursor-pointer p-1 ${className}`}
           src={session.user.image}
           alt="profile picture"
         />
@@ -17,7 +17,7 @@ export default function User() {
   return (
     <>
       <button
-        className="bg-blue-500 text-white px-6 py-2 font-medium rounded-md hover:brightness-105 hover:shadow-md"
+        className={`bg-blue-500 text-white px-6 py-2 font-medium rounded-md hover:brightness-105 hover:shadow-md ${className}`}
         onClick={signIn}
       >
         Sign in
